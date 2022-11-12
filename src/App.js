a1b489e6f38eb723c611d343b38d1013347d16d3
unknown <davit.khachaturov.1@iliauni.edu.ge>
2022-11-12 04:58:56 +0400

    made all filters, saved dark/light mode in local storage, made separate components for countries cards

--- src/App.js
+++ src/App.js
@@ -2,32 +2,69 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import { ReactComponent as DarkIcon } from './svg/moon.svg'; //icon
 import { ReactComponent as LightIcon } from './svg/sun.svg'; //icon
+import { ReactComponent as SearchIcon } from './svg/search_icon.svg'; //icon
+import { ReactComponent as ChevronDown } from './svg/chevronDown.svg'; //icon
+import CountriesCards from './components/countries_cards';
 
 function App() {
 
-  const [mode, setMode] = useState(true);
+  const regions = ['Africa', 'Antarctic', 'America', 'Asia', 'Europe', 'Oceania'];
+  const [mode, setMode] = useState(JSON.parse(localStorage.getItem("mode")));
   const [api, setApi] = useState([]);
-  const [regions, setRegions] = useState([]);
   const [search, setSearch] = useState('');
+  const [letter, setLetters] = useState([]);
+  const [regionSelect, setRegionSelect] = useState(false);
+  const [regionFilter, setRegionFilter] = useState('Filter by region');
+  const [filterBy, setFilterBy] = useState({
+    bySearch: false,
+    byRegion: false,
+    byLetter: false
+  })
 
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags')
       .then(res => res.json())
-      .then(json => {
-        setApi(json.sort((a, b) => a.name.common === b.name.common ? 0 : a.name.common < b.name.common ? -1 : 1));
-        setRegions(new Set([...json.map(item => item.region)]))
-      })
+      .then(json => setApi(json.sort((a, b) => a.name.common === b.name.common ? 0 : a.name.common < b.name.common ? -1 : 1)))
       .catch(e => console.log(e.message))
   }, []);
 
-  console.log(search);
+  useEffect(() => {
+    setLetters(Array.from({ length: 90 - 65 + 1 }, (_, i) => 65 + i).map(item => String.fromCharCode(item)));
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('mode', JSON.stringify(mode));
+  }, [mode]);
+
+
+  function searchFunc(e) {
+    const val = e.target.value.toLowerCase();
+    setSearch(val.trim());
+    setRegionFilter('Filter by region');
+    setFilterBy({ bySearch: true, byRegion: false, byLetter: false });
+  }
+
+  function letterFunc(e) {
+    setSearch(e.target.textContent);
+    setRegionFilter('Filter by region');
+    setFilterBy({ bySearch: false, byRegion: false, byLetter: true });
+  }
+
+  function regionFunc(e) {
+    const val = e.target.textContent;
+    setSearch(val);
+    setRegionFilter(val);
+    setFilterBy({ bySearch: false, byRegion: true, byLetter: false });
+  }
+
+  {/* <button onClick={() => window.history.back()}>go back</button> */ }
 
   return (
     <>
-      <nav className={mode ? 'bar' : 'bar bar_dark_mode'}>
+      <nav className={mode || mode === null ? 'bar' : 'bar bar_dark_mode'}>
         <h2>Where in the world?</h2>
         <div className="colormode" onClick={() => setMode(prev => !prev)}>
-          <div className={mode ? 'effect' : 'effect effect2'}></div>
+          <div className={mode || mode === null ? 'effect' : 'effect effect2'}></div>
           {mode ? <>
             <DarkIcon />
             <p>Dark Mode</p>
@@ -37,23 +74,24 @@ function App() {
           </>}
         </div>
       </nav>
-      <div className={mode ? 'content' : 'content content_dark_mode'}>
+      <div className={mode || mode === null ? 'content' : 'content content_dark_mode'}>
         <div className='content_inputs'>
-          <input type="search" placeholder='Search for a country...' onChange={(e) => setSearch(e.target.value)} />
-          <select id="dropdown" className="form-control" required>
-            <option defaultValue hidden>Filter by region</option>
-            {[...regions].map((item, index) => <option key={index} value={item}>{item}</option>)}
-          </select>
+          <span><SearchIcon /><input type="search" placeholder='Search for a country...' onChange={searchFunc} /></span>
+          <span className='letter_search' onClick={letterFunc}>
+            {letter.map((item, index) => <span key={index}>{item}</span>)}
+          </span>
+          <div className='dropdown' >
+            <div className='title' onClick={() => setRegionSelect(prev => !prev)}>
+              <p>{regionFilter}</p>
+              <ChevronDown />
+            </div>
+            {regionSelect ? <div className='drop_menu' onClick={regionFunc}>
+              {regions.map((item, index) => <div className='regions' key={index} value={item}>{item}</div>)}
+            </div> : null}
+          </div>
         </div>
         <div className='countries'>
-          {api.map((item, index) => <a href={`/${item.name.common}`} className='country_card' key={index}>
-            <img src={item.flags.png} alt={`${item.name.common}_flag`} />
-            <h3 onMouseOver={(e) => Object.keys(item.name.nativeName).length > 0 ? e.target.textContent = item.name.nativeName[Object.keys(item.name.nativeName)[0]].official : null} onMouseOut={(e) => e.target.textContent = item.name.common}>{item.name.common}</h3>
-            <p>Population: {item.population}</p>
-            {/* split-join */}
-            <p>Region: {item.region}</p>
-            <p>Capital: {item.capital.join(', ')}</p>
-          </a>)}
+          {api.map((item, index) => <CountriesCards item={item} key={index} search={search} filterBy={filterBy} />)}
         </div>
       </div>
     </>
---
+++ src/components/countries_cards.jsx
@@ -0,0 +1,19 @@
+function CountriesCards({ item, search, filterBy }) {
+    const exp = (item.name.common.toLowerCase().search(search) === -1 && filterBy.bySearch)
+        || (item.name.common[0] !== search && filterBy.byLetter) || (item.region.search(search) === -1 && filterBy.byRegion) ? 'country_card hide' : 'country_card';
+
+    return (
+        <a href={`/${item.name.common}`}
+            className={exp}>
+            <img src={item.flags.png} alt={`${item.name.common}_flag`} />
+            <div className="main_info">
+                <h3 onMouseOver={(e) => Object.keys(item.name.nativeName).length > 0 ? e.target.textContent = item.name.nativeName[Object.keys(item.name.nativeName)[0]].official : null} onMouseOut={(e) => e.target.textContent = item.name.common}>{item.name.common}</h3>
+                <p>Population: <span>{item.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</span></p>
+                <p>Region: <span>{item.region}</span></p>
+                <p>Capital: <span>{item.capital.join(', ')}</span></p>
+            </div>
+        </a>
+    )
+}
+
+export default CountriesCards;
