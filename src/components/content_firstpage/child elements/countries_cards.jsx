054096dedcd33fd409d7ff25bd0ff1820047232e
unknown <davit.khachaturov.1@iliauni.edu.ge>
2022-11-15 19:11:42 +0400

    test

--- src/components/content_firstpage/child elements/countries_cards.jsx
+++ src/components/content_firstpage/child elements/countries_cards.jsx
@@ -1,9 +1,9 @@
 function CountriesCards({ item, search, filterBy }) {
     const exp = (item.name.common.toLowerCase().search(search) === -1 && filterBy.bySearch)
         || (item.name.common[0] !== search && filterBy.byLetter) || (item.region.search(search) === -1 && filterBy.byRegion) ? 'country_card hide' : 'country_card';
-
+    //1
     return (
-        <a href={`/${item.name.common}`}
+        <a href={`${item.name.common}`}
             className={exp}>
             <img src={item.flags.png} alt={`${item.name.common}_flag`} />
             <div className="main_info">
